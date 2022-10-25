73601edc16bab1bc3c5cb04dd049c4bb2c253826
TichFi <admin@tich.fi>
2022-10-25 22:18:09 +0300

    Create navigation.tsx

---
+++ components/navigation.tsx
@@ -0,0 +1,51 @@
+import Link from 'next/link'
+import React from 'react'
+import Image from 'next/image'
+import styles from '../assets/scss/modules/Navigation.module.scss'
+
+interface page {
+  url: string;
+  name: string;
+  img_url: string;
+}
+
+// interface svg {
+//   d: string;
+//   box: string;
+// }
+
+const pages: page[] = [
+  {
+    url: '/',
+    name: 'Home',
+    img_url: '/home.svg'
+  },
+  {
+    url: '/shifts',
+    name: 'Shifts',
+    img_url: '/vercel.svg'
+  }
+]
+
+const Navigation: React.FC = () => {
+  return (
+    <>
+      <nav className={styles.nav}>
+        {
+          pages.map(page => {   // for each page on the list it will create a button to navigate to the page
+            return (
+              <Link href={page.url}>
+                <a className={styles.link}>
+                  <p>{page.name}</p>                          
+                  <img src={page.img_url} className={styles.svg}/>
+                </a>
+              </Link>
+            )
+          })
+        }
+      </nav>
+    </>
+  )
+}
+
+export default Navigation
