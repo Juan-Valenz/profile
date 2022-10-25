20a6958d0b4b89f872046df76e82127e08895653
Juan Valenzuela <juan.valenzuela@tich.fi>
2022-10-25 12:23:48 +0300

    Added a common layout to all pages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,13 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <header></header>
+      <Component {...pageProps} />
+      <footer></footer>
+    </>
+  )
 }
 
 export default MyApp
