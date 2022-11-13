31e40c11094170490d877e07205600f263596f9e
Juan Valenzuela <juan.valenzuela@tich.fi>
2022-11-13 15:51:25 +0200

    Create education.tsx

---
+++ pages/education.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Head from 'next/head'
+
+interface EducationProps {
+  
+}
+
+const Education: React.FC<EducationProps> = ({}) => {
+  return (
+    <>
+      <Head>
+        <title>page_title</title>
+        <meta name='author' content='Author' />
+        <meta name='description' content='page_description' />
+        <meta name='keywords' content='page_keywords' />
+      </Head>
+    </>
+  );
+}
+
+
+
+export default Education
