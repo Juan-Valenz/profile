f958d3760e31578059bb49d84f72f3016e1b1ce8
Juan Valenzuela <juan.valenzuela@tich.fi>
2022-11-13 15:53:22 +0200

    Update index.tsx
    •  Added the introduction component.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,23 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 // import Image from 'next/image'
-// import styles from '../styles/Home.module.css'
+import styles from 'styles/index.module.scss'
+import Introduction from '../components/introduction/introduction'
 
 const Home: NextPage = () => {
   return (
     <>
       <Head>
+        {/* <body className='light'/> */}
         <title>Juan Valenz | Portfolio</title>
         <meta name="description" content="General Description about myself, my work and hobbies." />
         <meta name="keywords" content="enterprise website development, bussiness website development ,sanitary website development,UI ,UX ,Website developer ,Website ,Cleaning Service ,freelancer ,web developer ,programmer,Portfolio,HTML, CSS, JavaScript, enterprise, business" />
         <meta name="author" content="Juan Valenzuela" />
         {/* <link rel="icon" href="/favicon.ico" /> */}
       </Head>
-
+      <header>
+        <Introduction/>
+      </header>
     </>
   )
 }
