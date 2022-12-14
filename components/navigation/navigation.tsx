76f42e5f8c2e83acceaee8c3eb664a6ed2ee1ea9
Juan Valenzuela <juan.valenzuela@tich.fi>
2022-12-14 13:49:29 +0200

    Update navigation.tsx

--- components/navigation/navigation.tsx
+++ components/navigation/navigation.tsx
@@ -1,57 +1,50 @@
-import Link from 'next/link'
-import React from 'react'
-import Image from 'next/image'
+import React, { useState } from 'react'
 import styles from './navigation.module.scss'
 import { page } from '../types'
+import Link, { PopUpLink } from '../link/link'
+import Image from 'next/image'
+import { useModalContext } from '@rintsin/common-components'
+import Settings from '../settings/settings'
+import { timeout } from '../functions'
 
 const pages: page[] = [
   {
     url: '/',
-    name: 'Home',
-    img: {
-      url: '/home.svg',
-      alt: 'home icon',
-      width: 20,
-      height: 20
-    }
+    name: 'Home'
   },
   {
     url: '/education',
-    name: 'Education',
-    img: {
-      url: '/education.svg',
-      alt: 'education icon',
-      width: 20,
-      height: 20
-    }
+    name: 'Education'
   },
   {
     url: '/repositories',
-    name: 'Projects',
-    img: {
-      url: '/github.svg',
-      alt: 'github icon',
-      width: 20,
-      height: 20
-    }
+    name: 'Projects'
   }
 ]
 
-const Navigation: React.FC = () => (
-  <nav className={styles.nav}>
-    {
-      pages && pages.map(page => <PageLink key={page.url} url={page.url} name={page.name} img={page.img} />)
-    }
-  </nav>
-)
 
-const PageLink: React.FC<page> = ({ url, name, img }) => (
-  <Link href={url}>
-    <a className={styles.link}>
-      <p>{name}</p>
-      <Image src={img.url} alt={img.alt} width={img.width} height={img.height} />
-    </a>
-  </Link>
-)
+const Navigation: React.FC = () => {
+  const [expanded, setExpanded] = useState(false);
+
+  const { modal } = useModalContext();
+  const display = () =>
+    modal({
+      showCloseOnRightCorner: true,
+      content: <Settings />,
+    });
+  return (
+    <>
+      <nav className={`${styles.nav}`} aria-expanded={expanded}>
+        {
+          pages && pages.map(page => <Link key={page.url} url={page.url} name={page.name} onClick={async()=>{ await timeout(200) ;setExpanded(e=>!e)}} />)
+        }
+        <PopUpLink onClick={()=>{display(); setExpanded(e=>!e)}} name={'Settings'} />
+      </nav>
+      <button className={styles.icon} aria-expanded={expanded} onClick={() => setExpanded(e => e = !e)}>
+        <Image src={expanded ? '/icons/x.svg' : '/icons/burger.svg'} alt='expand navigation icon' layout='fill' />
+      </button>
+    </>
+  )
+}
 
 export default Navigation
