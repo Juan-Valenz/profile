1936f468179e24c18da0bfc0209a6c3fc43aa5c9
Juan Valenzuela <juan.valenzuela@tich.fi>
2022-12-14 13:50:26 +0200

    Create repositoryCard.tsx

---
+++ components/repositoryCard/repositoryCard.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+interface RepositoryCardProps {
+    name?: string;
+    description?: string;
+    topics?: string[];
+    languages?: coding_language[];
+    urls?: urls;
+    updated?: Date;
+    created?: Date;
+}
+
+const RepositoryCard: React.FC<RepositoryCardProps> = ({}) => {
+    return ();
+}
+
+
+
+export default RepositoryCard
