30059722e509e3d3b2960cee15aac10a2124698b
Juan Valenzuela <juan.valenzuela@tich.fi>
2022-12-14 15:57:16 +0200

    Update repositories.ts
    • attempting to change the object key names.

--- pages/api/repositories.ts
+++ pages/api/repositories.ts
@@ -2,18 +2,37 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 import { IRepo, repository } from '../../types/github'
 
 const gitHubUsername = 'Juan-Valenz'
-const GitHubReposUrl = `https://api.github.com/users/${gitHubUserName}/repos`
+const GitHubReposUrl = `https://api.github.com/users/${gitHubUsername}/repos`
 
 type Data = {
-  repos: IRepo[]
+    repos: repository[]
 }
 
 const handler = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
 
-  const data: IRepo[] = await fetch(GitHubReposUrl).then(x => x.json())
-  const repository: repository
-  res.status(200).json({repos: data})
-  res.end()
+    const repos: IRepo[] = await fetch(GitHubReposUrl).then(x => x.json())
+    const repositories: repository[] = [];
+
+    repos.forEach(r => {
+        repositories.push({
+            name: r.name
+        })
+    });
+
+    res.status(200).json({ repos: repositories })
+    res.end()
 }
+
+// const get_languages = async (url?: string): Promise<coding_language[] | undefined> => {
+//     const languages = url ? await fetch(url).then(d => d.json()) : null
+//     const coding_languages: coding_language[] = [];
+//     for (var key in languages) {
+//         coding_languages.push({
+//             language: key,
+//             amount: languages[key]
+//         })
+//     }
+//     return coding_languages;
+// }
 
 export default handler
