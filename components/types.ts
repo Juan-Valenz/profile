e8ba262b48495fff0a9fbf91ba20a5038f9a7553
Juan Valenzuela <juan.valenzuela@tich.fi>
2022-12-14 13:49:49 +0200

    Update types.ts

--- components/types.ts
+++ components/types.ts
@@ -1,21 +1,20 @@
+import { parse } from "path";
+import { useEffect, useState } from "react";
 
 
 // navigation | routing
 export interface page {
     url: string;
     name: string;
-    img: img;
 }
 
 export interface svg {
-  d: string;
-  box: string;
+    d: string;
+    box: string;
 }
 export interface img {
     url: string;
     alt: string;
-    width: number;
-    height: number;
 }
 
 // github repositories
@@ -36,3 +35,13 @@ export interface repository {
     languages: coding_language[]
     topics: string[]
 }
+
+    // const [width, setWidth] = useState<number>();
+    // useEffect(() => {
+    //     const handleResize = (event: UIEvent) => {
+    //         const width = event.view?.innerWidth;
+    //         width && width > 400 && width < 430 && setWidth(w => w = window.innerWidth);
+    //     }
+    //     window.addEventListener("resize", handleResize);
+    //     return () => window.removeEventListener("resize", handleResize);
+    // }, []);
