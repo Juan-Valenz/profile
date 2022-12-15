33ccd4cbfda6e5923fd94802c251f44b57a97509
Juan Valenzuela <juan.valenzuela@tich.fi>
2022-12-15 14:41:03 +0200

    Update _app.tsx
    Changed the themeHooks file name

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,7 @@ import '../assets/styles/global.scss';
 import type { AppProps } from 'next/app';
 import Layout from '../components/layout/layout';
 import { ModalContextProvider } from '@rintsin/common-components';
-import { ThemeProvider } from '../components/ThemeHook';
+import { ThemeProvider } from '../components/themeHook';
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
   <ThemeProvider>
