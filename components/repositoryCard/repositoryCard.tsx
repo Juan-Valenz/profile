acca95f1464c14ce78b2bdbb5541f5199e136bd4
Juan Valenzuela <juan.valenzuela@tich.fi>
2022-12-15 09:28:12 +0200

    Update repositoryCard.tsx

--- components/repositoryCard/repositoryCard.tsx
+++ components/repositoryCard/repositoryCard.tsx
@@ -1,47 +1,70 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import styles from './repositoryCard.module.scss';
-import { coding_language, urls } from '../types';
+import { coding_language, urls } from '../../types/github';
 
 interface RepositoryCardProps {
     name?: string;
     description?: string;
     topics?: string[];
-    languages?: coding_language[];
+    languages_url?: string;
     urls?: urls;
-    updated?: Date;
-    created?: Date;
+    updated?: string;
+    created?: string;
 }
 
-const RepositoryCard: React.FC<RepositoryCardProps> = ({ name, description, topics, languages, urls, updated, created }) => {
+const RepositoryCard: React.FC<RepositoryCardProps> = ({ name, description, topics, languages_url, urls, updated, created }) => {
+
+    const [languages, setLanguages] = useState<coding_language[]>([])
+    useEffect(() => {
+        fetch('api/languages', { body: JSON.stringify({ "language_url": languages_url }) })
+            .then(res => res.json())
+            .then(data => {
+                setLanguages(data.languages)
+            })
+            .catch(err => console.error(err.message))
+    }, [])
     return (
         <div className={styles.container}>
             <header>
                 <h3>{name}</h3>
                 <p>{description}</p>
             </header>
-            <div className={styles.links}>
-                <h4>Links</h4>
-                <div>
-                    <a href={urls?.website}>
-                        <img src="" alt="" />
-                    </a>
-                    <a href={urls?.github}>
-                        <img src="" alt="" />
-                    </a>
+            <section>
+                <div className={styles.topics}>
+                    <h4>Topics</h4>
+                    <div>
+                        {topics?.map(t => <div>{t.toString()}</div>)}
+                    </div>
                 </div>
-            </div>
-            <div className={styles.topics}>
-                <h4>Topics</h4>
-                <div></div>
-            </div>
-            <div className={styles.languages}>
-                <h4>Languages</h4>
-                <div></div>
-            </div>
-            <div className={styles.dates}>
-                <p>Created: <time dateTime={created?.toDateString()}>{created?.toString()}</time></p>
-                <p>Updated: <time dateTime={updated?.toDateString()}>{updated?.toString()}</time></p>
-            </div>
+                <div className={styles.languages}>
+                    <h4>Languages</h4>
+                    <div>
+                        {languages.map(l =>
+                            <div key={l.language}>
+                                <p>{l?.language}</p>
+                                <p>{JSON.stringify(l.amount)}</p>
+                            </div>)}
+                    </div>
+                </div>
+            </section>
+            <section>
+
+                <div className={styles.links}>
+                    <h4>Links</h4>
+                    <div>
+                        <a href={urls?.github}>
+                            <img src="/icons/github.svg" alt="" />
+                        </a>
+                        {urls?.website && <a href={urls?.website}>
+                            <img src="/icons/globe.svg" alt="" />
+                        </a>}
+                    </div>
+                </div>
+                <div className={styles.dates}>
+                    <p>Created: {created && new Date(created).toDateString()}</p>
+                    <p>Updated: {updated && new Date(updated).toDateString()}</p>
+                </div>
+            </section>
 
         </div>
     );
