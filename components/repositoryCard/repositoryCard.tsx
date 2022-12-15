16cea8de88e72d7c0db09ea1e4b86db8b53185f7
Juan Valenzuela <juan.valenzuela@tich.fi>
2022-12-15 14:06:28 +0200

    Update repositoryCard.tsx
    set links target to _blank ( to open in new tab)

--- components/repositoryCard/repositoryCard.tsx
+++ components/repositoryCard/repositoryCard.tsx
@@ -53,10 +53,10 @@ const RepositoryCard: React.FC<RepositoryCardProps> = ({ name, description, topi
             <section>
                 <div className={styles.links}>
                     <div>
-                        <a href={urls?.github}>
+                        <a href={urls?.github} target='_blank'>
                             <Image title='Github' src="/icons/github.svg" alt="" layout='fill' />
                         </a>
-                        {urls?.website && <a href={urls?.website}>
+                        {urls?.website && <a href={urls?.website} target='_blank'>
                             <Image title='website' src="/icons/globe.svg" alt="" width={15} layout='fill' />
                         </a>}
                     </div>
