163bf3dcb9e844b1e0b4c6e97eff6f3d1444ab86
Juan Valenzuela <juan.valenzuela@tich.fi>
2022-12-15 13:38:05 +0200

    Update introduction.tsx
    • change img element to 'next/image' component.

--- components/introduction/introduction.tsx
+++ components/introduction/introduction.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import styles from './introduction.module.scss'
+import Image from 'next/image'
 
-const Introduction: React.FC = ({ }) => (
+
+const Introduction: React.FC = () => (
   <section className={styles.container}>
-    <img className={styles.image} src="/images/cloud_server.png" alt="Cloud Service image" />
+    <div className={styles.image}>
+      <Image src="/images/cloud_server.png" alt="Cloud Service image" layout='fill' />
+    </div>
     <h2>
       <span>Let's set the building</span>
       <span>blocks of your cloud</span>
