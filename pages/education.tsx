6cc758c88738dac3e74fe9bc46ecb387cf761f4d
Juan Valenzuela <juan.valenzuela@tich.fi>
2022-12-15 13:15:29 +0200

    Update education.tsx

--- pages/education.tsx
+++ pages/education.tsx
@@ -1,19 +1,37 @@
 import React from 'react'
 import Head from 'next/head'
+import styles from 'assets/styles/education.module.scss'
+import EducationCard from '../components/educationCard/educationCard';
 
-interface EducationProps {
+
+
+const educations = [
+  {
+  school: "Varia",
+  school_url: "https://varia.vantaa.fi/fi/palveluhakemisto/palvelu/tieto-ja-viestintatekniikan-perustutkinto#tab-introduction",
+  description: "Varia teaches several ICT orientations and one of them, which is the one I took, is programming.",
+  start: new Date()
+  }
+]
+
+const Education: React.FC = () => {
   
-}
-
-const Education: React.FC<EducationProps> = ({}) => {
   return (
     <>
       <Head>
-        <title>page_title</title>
-        <meta name='author' content='Author' />
-        <meta name='description' content='page_description' />
-        <meta name='keywords' content='page_keywords' />
+        <title>Juan Valenz | Education</title>
+        <meta name="description" content="My studies." />
+        <meta name="keywords" content="education, schools " />
+        <meta name="author" content="Juan Valenzuela" />
       </Head>
+      <section className={styles.container}>
+        <header>
+          <h2>Education</h2>
+        </header>
+        <div>
+          {educations.map(e => <EducationCard key={e.school} school={e.school} school_url={e.school_url} description={e.description} start={e.start}/>)}
+        </div>
+      </section>
     </>
   );
 }
